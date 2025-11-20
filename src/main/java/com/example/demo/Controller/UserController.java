fd229f2d3df9e8d9a9be77d3dbc70d36457a3e11
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-11-20 23:20:41 +0530

    feat: Login api set

--- src/main/java/com/example/demo/Controller/UserController.java
+++ src/main/java/com/example/demo/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.demo.Controller;
 
 import com.example.demo.Models.User;
 import com.example.demo.Repository.UserRepository;
+import com.example.demo.security.TokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,12 +11,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private TokenGenerator tokenGenerator;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -26,5 +31,14 @@ public class UserController {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepository.save(user);
         return ResponseEntity.ok("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody User user) {
+        String token = tokenGenerator.generateToken(user.getEmail(), user.getPassword());
+        if (token != null) {
+            return ResponseEntity.ok(Map.of("token", token));
+        }
+        return ResponseEntity.status(401).body("Invalid credentials");
     }
 }
