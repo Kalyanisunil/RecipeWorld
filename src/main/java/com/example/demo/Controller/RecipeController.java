cf709aa634cd8beaf1624756f8127132e2180599
Kalyani Sunil Kumar <kalyanisunil247@gmail.com>
2025-11-22 21:25:08 +0530

    feat: Add post api for adding recipe

--- src/main/java/com/example/demo/Controller/RecipeController.java
+++ src/main/java/com/example/demo/Controller/RecipeController.java
@@ -6,10 +6,7 @@ import com.example.demo.Models.Recipe;
 import com.example.demo.Models.User;
 import com.example.demo.Repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +28,13 @@ public class RecipeController {
         Recipe recipe = repo.findById(id)
                 .orElseThrow(() -> new RecipeNotFoundException(id));
         return List.of(recipe);
+    }
+
+
+
+    @PostMapping("addRecipe")
+    Recipe newRecipe(@RequestBody Recipe newRecipe) {
+        return repo.save(newRecipe);
     }
 
 }
